625a910bb67b3bac6bd07ef5de0a2c0bdbb0e3de
Tej <dahaltn@gmail.com>
2021-12-30 03:08:21 +0000

    ....Testing.....more..yes

--- build.sh
+++ build.sh
@@ -10,8 +10,9 @@ if [ ! -f "$FILE" ]; then
     cp src/sites/default/settings.php drupal/web/sites/default/settings.php
 fi
 
+docker-compose down
 docker-compose up -d
-docker-compose exec web composer install  --ignore-platform-reqs -n
+docker-compose exec web composer install --ignore-platform-reqs -n
 
 #docker-compose exec web drush cim -y
 #docker-compose composer id -n
