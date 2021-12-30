9326de8cb0857fa6ab8f1d5e4de1e37b66a2d090
Tej <dahaltn@gmail.com>
2021-12-30 23:35:25 +0000

    progress...

--- build.sh
+++ build.sh
@@ -10,14 +10,20 @@ FILE=$PWD/drupal/web/sites/default/settings.php
 if [ ! -f "$FILE" ]; then
     cp $PWD/src/sites/default/settings.php $FILE
    chmod 644 $FILE
+else
+     chmod 644 $FILE
 fi
 
 FILE_FOLDER=$PWD/drupal/web/sites/default/files
 
 if [ ! -d "$FILE_FOLDER" ]; then
- mkdir -p $FILE_FOLDER && chown -R dahaltn:dahaltn $FILE_FOLDER
- chmod 755 $PWD/drupal/web/sites/default
- chmod -R 777 $FILE_FOLDER
+   mkdir -p $FILE_FOLDER && chown -R dahaltn:dahaltn $FILE_FOLDER
+   chmod 755 $PWD/drupal/web/sites/default
+   chmod -R 777 $FILE_FOLDER
+ else
+    chmod 755 $PWD/drupal/web/sites/default
+    chmod -R 777 $FILE_FOLDER
+
 fi
 
 #    docker exec dahal_drupal9 composer install -n
