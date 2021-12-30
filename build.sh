6e7cdc72f7e23200fb381d3f05dbcedfe952417a
Tej <dahaltn@gmail.com>
2021-12-30 02:36:25 +0000

    Testing

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+FILE=.env
+if [ ! -f "$FILE" ]; then
+    cp .env.example .env
+fi
+
+docker-compose up -d
+docker-compose exec web composer install  --ignore-platform-reqs -n
+
+docker-compose exec web drush cim -y
+#docker-compose composer id -n
