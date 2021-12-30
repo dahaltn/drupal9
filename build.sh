4b96d08910f00567528cc82fb1df93f6dda73c62
Tej <dahaltn@gmail.com>
2021-12-30 23:22:55 +0000

    workflow improvement

--- build.sh
+++ build.sh
@@ -8,7 +8,17 @@ fi
 
 FILE=$PWD/drupal/web/sites/default/settings.php
 if [ ! -f "$FILE" ]; then
-    cp $PWD/src/sites/default/settings.php drupal/web/sites/default/settings.php
+    cp $PWD/src/sites/default/settings.php $FILE
+   chmod 644 $FILE
 fi
+
+FILE_FOLDER=$PWD/drupal/web/sites/default/files
+
+if [ ! -d "$FILE_FOLDER" ]; then
+ mkdir -p $FILE_FOLDER && chown -R dahaltn:dahaltn $FILE_FOLDER
+ chmod 755 $PWD/drupal/web/sites/default
+ chmod -R 777 $FILE_FOLDER
+fi
+
 #    docker exec dahal_drupal9 composer install -n
 #    docker exec dahal_drupal9 composer id -n
