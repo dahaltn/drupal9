2036bc5c1f539edf70241b4a5296b141e4bcf1b9
Tej <dahaltn@gmail.com>
2021-12-31 15:43:23 +0000

    building

--- build.sh
+++ build.sh
@@ -5,17 +5,6 @@ if [ ! -f "$FILE" ]; then
     cp .env.example .env
 fi
 
-if [ ! "$(docker ps -q -f name=mysql_mysql_database)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=mysql_database)" ]; then
-        # cleanup
-        docker rm mysql_database
-    fi
-    # run containers
-    cd /var/www
-    docker-compose up -d
-else
-  docker exec dahal_drupal9 composer install -n
-fi
 
 FILE=drupal/web/sites/default/settings.php
 if [ ! -f "$FILE" ]; then
@@ -36,9 +25,23 @@ if [  -d "$CONFIG_FOLDER" ]; then
    chown -R www-data:www-data $CONFIG_FOLDER
 fi
 
+if [ ! "$(docker ps -q -f name=mysql_mysql_database)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=mysql_database)" ]; then
+        # cleanup
+        docker rm mysql_database
+    fi
+    # run containers
+    cd /var/www
+    docker-compose up -d
+else
+  docker exec dahal_drupal9 composer install -n
+  docker exec dahal_drupal9 drush cim -y
+fi
 #   DB_NAME=drupal9
 #   docker exec mysql_database /usr/bin/mysqldump -u root --password=newnew drupal9 > drupal9_initial_setup_backup.sql
 #   docker exec -i mysql_database mysql -u root --password=newnew drupal9 < drupal9_initial_setup_backup.sql
 
 #    docker exec dahal_drupal9 composer install -n
 #    docker exec dahal_drupal9 composer id -n
+#    docker exec dahal_drupal9 drush cim -y
+#    docker exec dahal_drupal9 drush cex -y
