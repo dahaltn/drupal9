53027977efa5b6c7e302443061ff6e0871576cc5
Tej <dahaltn@gmail.com>
2021-12-31 10:52:53 +0000

    Setup deployment workflow

--- build.sh
+++ build.sh
@@ -1,29 +1,27 @@
 #!/bin/sh
-cd $PWD
-#cd /var/www
+
 FILE=.env
 if [ ! -f "$FILE" ]; then
     cp .env.example .env
 fi
 
-FILE=$PWD/drupal/web/sites/default/settings.php
+FILE=drupal/web/sites/default/settings.php
 if [ ! -f "$FILE" ]; then
-    cp $PWD/src/sites/default/settings.php $FILE
-   chmod 644 $FILE
-else
-     chmod 644 $FILE
+    cp src/sites/default/settings.php $FILE
 fi
+     chmod 644 $FILE
 
-FILE_FOLDER=$PWD/drupal/web/sites/default/files
+FILE_FOLDER=drupal/web/sites/default/files
 
 if [ ! -d "$FILE_FOLDER" ]; then
    mkdir -p $FILE_FOLDER && chown -R dahaltn:dahaltn $FILE_FOLDER
-   chmod 755 $PWD/drupal/web/sites/default
-   chmod -R 777 $FILE_FOLDER
+   chown -R www-data:www-data drupal/web/sites/default
+   chmod 755 drupal/web/sites/default
+   chmod -R 755 $FILE_FOLDER
  else
-    chmod 755 $PWD/drupal/web/sites/default
-    chmod -R 777 $FILE_FOLDER
-
+    chmod 755 drupal/web/sites/default
+    chown -R www-data:www-data drupal/web/sites/default
+    chmod -R 755 $FILE_FOLDER
 fi
 
 #    docker exec dahal_drupal9 composer install -n
